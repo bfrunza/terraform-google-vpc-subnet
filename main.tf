4d25503eb8f1b853204801901c43fa7a3e4cf28c
BF <bfrunza@anvibo.com>
2019-11-18 11:32:23 +0200

    updating syntax to 0.12 version

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 
 resource "google_compute_subnetwork" "subnet" { 
-  name          = "${var.subnet_name}"
-  ip_cidr_range = "${var.subnet_cidr}"
-  region        = "${var.subnet_region}"
-  network       = "${var.network}"
+  name          = var.subnet_name
+  ip_cidr_range = var.subnet_cidr
+  region        = var.subnet_region
+  network       = var.network
 }
 
 
--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,7 @@
 output "name" {
-  value = "${google_compute_subnetwork.subnet.name}"
+  value = google_compute_subnetwork.subnet.name
 }
 
 output "self_link" {
-  value = "${google_compute_subnetwork.subnet.self_link}"
+  value = google_compute_subnetwork.subnet.self_link
 }
